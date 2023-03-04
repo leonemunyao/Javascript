ada922fa092cea2bd16149d2df9ac0a0aab31214
leone <leonemunyao>
2023-03-04 16:23:22 +0300

    First commit

---
+++ Main.js
@@ -0,0 +1,16 @@
+  // My first Javascript code
+  console.log('Hello World');
+  let name = 'Leone';
+  console.log(name);
+  let Firstname ='Leone', Lastname ='Munyao';
+  console.log(Firstname, Lastname);
+
+  let person ={
+    name: 'Leon',
+    age: 22
+  }
+
+  console.log(person);
+
+  person.name = 'LEON';
+  console.log(person);
